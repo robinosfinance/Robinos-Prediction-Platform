bcf4a8c0d7489cc6a5e57e53b23da9ac22ec34ff
Filip Ivanovic <fivanovic98@gmail.com>
2021-11-28 21:05:47 +0100

    moved randomInt function to helper

--- helper.js
+++ helper.js
@@ -1,5 +1,11 @@
-const secondsInTheFuture = seconds => Math.floor(Date.now() / 1000) + seconds; 
+const secondsInTheFuture = seconds => Math.floor(Date.now() / 1000) + seconds;
+
+const randomInt = (min, max) => {
+    const diff = max - min;
+    return Math.ceil(Math.random() * diff) + min;
+};
 
 module.exports = {
     secondsInTheFuture,
+    randomInt,
 };
