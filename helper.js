64f3c48e6b4dc1de0e7747b79b2d303d495fbe6a
filip-ivanovic <93095135+filip-ivanovic@users.noreply.github.com>
2022-02-20 16:09:21 +0100

    added useMethodsOn helper function

--- helper.js
+++ helper.js
@@ -15,9 +15,43 @@ const idsFrom = (fromId, length) => {
 
 const timeInSecs = () => Math.round(Date.now() / 1000);
 
+const useMethodsOn = (contractInstance, methods) => {
+  if (methods.length === 0) return Promise.resolve();
+
+  const recursiveFunction = (methodIndex, promise) =>
+    promise.then(async (previousReturnValue) => {
+      const { name, args, account, onReturn } = methods[methodIndex];
+      const requestInstance = contractInstance.methods[name](...args)[
+        onReturn ? 'call' : 'send'
+      ]({
+        from: account,
+        gas: '1000000000',
+      });
+
+      const returnValue = await requestInstance;
+      onReturn && onReturn(returnValue, previousReturnValue);
+      if (methods[methodIndex + 1])
+        return recursiveFunction(methodIndex + 1, requestInstance);
+      else return requestInstance;
+    });
+
+  return recursiveFunction(0, Promise.resolve());
+};
+
+const zeroOrOne = () => randomInt(0, 2) - 1;
+
+const newArray = (length, callback) => {
+  const array = [];
+  for (let i = 0; i < length; i++) array.push(callback(i));
+  return array;
+};
+
 module.exports = {
   secondsInTheFuture,
   randomInt,
   idsFrom,
   timeInSecs,
+  useMethodsOn,
+  zeroOrOne,
+  newArray,
 };
