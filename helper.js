ec72e1675fc369cd080baa2dc658d25aeacd0b6f
filip-ivanovic <filip.ivanovic@styliff.com>
2022-03-10 21:48:40 +0100

    added Debugger tool

--- helper.js
+++ helper.js
@@ -80,6 +80,29 @@ const formatArgs = (args) =>
     ''
   )})`;
 
+const printLogs = async (contractInstance) => {
+  const getLogs = async (contractInstance) => {
+    return (await contractInstance.getPastEvents('allEvents')).map(({
+      event,
+      returnValues
+    }) => {
+      const log = {
+        event
+      };
+      Object.entries(returnValues).forEach(([key, value]) => {
+        if (/^\d*$/.test(key)) return;
+        log[key] = value;
+      });
+      return log;
+    }).filter(log => ({
+      message
+    }) => !!message && /^Debugger: /.test(message));
+  };
+  // eslint-disable-next-line no-console
+  console.log(await getLogs(contractInstance));
+};
+
+
 module.exports = {
   secondsInTheFuture,
   randomInt,
@@ -89,4 +112,5 @@ module.exports = {
   zeroOrOne,
   newArray,
   deploy,
+  printLogs
 };
