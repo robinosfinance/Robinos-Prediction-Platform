355d929ddccddaad1efd8b76444abe4179c7f1bf
filip-ivanovic <filip.ivanovic@styliff.com>
2022-03-17 22:02:46 +0100

    removed Debugger

--- contracts/RobinosGovernanceTokenNFTSubscription.sol
+++ contracts/RobinosGovernanceTokenNFTSubscription.sol
@@ -1458,40 +1458,6 @@ contract RecordingStakingAndRewardTokens is Ownable, SaleFactory {
     }
 }
 
-abstract contract Debugger {
-    event LogUint(string message, uint256 num, address sender);
-    event LogString(string message, string str, address sender);
-    event LogBool(string message, bool boolean, address sender);
-    event LogAddress(string message, address addr, address sender);
-    event Log(string message, address sender);
-
-    string constant MESSAGE_PREFIX = "Debugger: ";
-
-    function logUint(string memory message, uint256 num) internal {
-        emit LogUint(concatStrings(MESSAGE_PREFIX, message), num, msg.sender);
-    }
-
-    function logString(string memory message, string memory str) internal {
-        emit LogString(concatStrings(MESSAGE_PREFIX, message), str, msg.sender);
-    }
-
-    function logBool(string memory message, bool boolean) internal {
-        emit LogBool(concatStrings(MESSAGE_PREFIX, message), boolean, msg.sender);
-    }
-
-    function logAddress(string memory message, address addr) internal {
-        emit LogAddress(concatStrings(MESSAGE_PREFIX, message), addr, msg.sender);
-    }
-
-    function log(string memory message) internal {
-        emit Log(concatStrings(MESSAGE_PREFIX, message), msg.sender);
-    }
-
-    function concatStrings(string memory a, string memory b) private pure returns (string memory) {
-        return string(abi.encodePacked(a, b));
-    }
-}
-
 /***********************************************************************
  ***********************************************************************
  *******        ROBINOS GOVERNANCE TOKEN NFT SUBSCRIPITION       *******
@@ -1502,8 +1468,7 @@ contract RobinosGovernanceTokenNFTSubscription is
     ERC721Receiver,
     HandlingTime,
     ArrayUtils,
-    RecordingStakingAndRewardTokens,
-    Debugger
+    RecordingStakingAndRewardTokens
 {
     struct UserSubscribedTimestamps {
         uint256 subscribedAt;
